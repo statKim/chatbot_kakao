3589b595a72b96077f9552bf9416260e7b29d4bf
Hyunsung Kim <hyunsung1021@naver.com>
2018-08-30 02:32:04 +0000

    echo bot

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json # json으로 바꾸기 위한 라이브러리
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def hello():
     return "챗봇 페이지입니다!!!"
     
+# keyboard API
 @app.route("/keyboard")
 def keyboard():
     # keyboard 딕셔너리 생성
@@ -19,5 +20,26 @@ def keyboard():
     # 딕셔너리를 json으로 바꿔서 return
     json_keyboard = json.dumps(keyboard)
     return json_keyboard
+
+# message API
+@app.route("/message", methods=["POST"])
+def message():
+    # content라는 key의 value를 msg에 저장
+    msg = request.json["content"] 
+    
+    # 카카오톡에서 명령어 입력시 "text"에 있는 문구가 출력됨!!
+    # 현재 이 단계에서는 사용자가 입력한 값을 그대로 답해줌
+    json_return = {
+        "message" : {
+            "text" : msg
+        },
+        "keyboard" : {
+            "type" : "buttons",
+            "buttons" : ["메뉴", "로또", "고양이", "영화"]
+        }
+    }
+    
+    # json.dumps()와 같은 기능 - json으로 바꿔주는 기능
+    return jsonify(json_return)
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
