a8f297376a0e5d171dd4637b89278268492ac3a9
Hyunsung Kim <hyunsung1021@naver.com>
2018-09-21 14:46:29 +0000

    revise kbo

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import os
 import json # json으로 바꾸기 위한 라이브러리
 import random
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from bs4 import BeautifulSoup
 import time # 영재관 메뉴에서 오늘의 요일 찾을 때 사용
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # "https://python-chatbot-hyunsung1021.c9users.io" : c9 ip주소(root page)
 def hello():
     return "챗봇 페이지입니다!!!"
     
@@ -48,7 +48,8 @@ def message():
         img_bool = True
         url = "https://api.thecatapi.com/v1/images/search?mime_types=jpg"
         req = requests.get(url).json()
-        img_url = req[0]["url"]
+        img_url = req[0]["url"] # ex) https://24.media.tumblr.com/tumblr_lq46ox3kqA1qmjao5o1_500.jpg
+        # print(img_url)
         return_msg = "고양이"
     elif msg == "영화":
         img_bool = True
@@ -74,7 +75,7 @@ def message():
         pick_rank = random.randrange(0,7)
         pick_movie = movie_dic[pick_rank]
         #print(pick_movie)
-        return_msg = "영화제목 : {} \n평점 : {} \n예매율 : {}\n예매순위 : {}".format(pick_movie["title"], pick_movie["star"], pick_movie["reserve"], pick_rank + 1)
+        return_msg = "영화제목 : {}\n평점 : {}\n예매율 : {}\n예매순위 : {}".format(pick_movie["title"], pick_movie["star"], pick_movie["reserve"], pick_rank + 1)
         img_url = pick_movie["img"]
     
     # "학식 메뉴" 선택시    
@@ -118,7 +119,40 @@ def message():
             
         breakfast = menu_list[ind_breakfast].text
         dinner = menu_list[ind_dinner].text
-        return_msg = "■ 오늘의 메뉴\n★아침\n{}\n★저녁\n{}".format(breakfast, dinner)        
+        return_msg = "■ 오늘의 메뉴\n\n★아침\n{}\n\n★저녁\n{}".format(breakfast, dinner)        
+    elif msg == "내일의 메뉴":
+        url = "http://www.jeju.go.kr/genius/notice/menu.htm"
+        req = requests.get(url).text
+        doc = BeautifulSoup(req, "html.parser")
+        menu_list = doc.select("#mainContents > div.module-wrapper > table.table.table-list.table-bordered.table-week > tbody > tr > td > p.menu")
+
+        week = ["월", "화", "수", "목", "금", "토", "일"]
+        tomorrow = week[time.localtime().tm_wday + 1]
+        if tomorrow == "월":
+            ind_breakfast = 1
+            ind_dinner = 8
+        elif tomorrow == "화":
+            ind_breakfast = 2
+            ind_dinner = 9
+        elif tomorrow == "수":
+            ind_breakfast = 3
+            ind_dinner = 10            
+        elif tomorrow == "목":
+            ind_breakfast = 4
+            ind_dinner = 11
+        elif tomorrow == "금":
+            ind_breakfast = 5
+            ind_dinner = 12            
+        elif tomorrow == "토":
+            ind_breakfast = 6
+            ind_dinner = 13            
+        elif tomorrow == "일":
+            ind_breakfast = 0
+            ind_dinner = 7            
+            
+        breakfast = menu_list[ind_breakfast].text
+        dinner = menu_list[ind_dinner].text
+        return_msg = "■ 내일의 메뉴\n\n★아침\n{}\n\n★저녁\n{}".format(breakfast, dinner)  
     elif msg == "월":
         url = "http://www.jeju.go.kr/genius/notice/menu.htm"
         req = requests.get(url).text
@@ -185,14 +219,17 @@ def message():
         home = doc.select("#todaySchedule > li > div.vs_rgt > p > strong")
         away_pit = doc.select("#todaySchedule > li > div.vs_lft > p > span > a")
         home_pit = doc.select("#todaySchedule > li > div.vs_rgt > p > span > a")
-        test = "★ 오늘의 경기\n\n"
+        test = "★ 오늘의 매치업\n\n"
         stadium = {"넥센":"고척", "SK":"문학", "두산":"잠실", "LG":"잠실", "KIA":"광주",
                     "롯데":"사직", "삼성":"대구", "NC":"마산", "한화":"대전", "KT":"수원"}
-        for i in range(5):
-            if home[i].text in stadium:
-                test = test + stadium[home[i].text] + "\n" + away[i].text + "\tVS\t" + home[i].text + "\n" + away_pit[i].text + "\tVS\t" + home_pit[i].text + "\n\n"
-        return_msg = test[:-2]
-    elif msg == "오늘의 경기 결과":
+        if len(away) != 0:
+            for i in range(len(away)):
+                if home[i].text in stadium:
+                    test = test + stadium[home[i].text] + "\n" + away[i].text + "\tVS\t" + home[i].text + "(♠)\n" + away_pit[i].text + "\tVS\t" + home_pit[i].text + "\n\n"
+            return_msg = test[:-2]
+        else:
+            return_msg = "오늘은 경기가 없습니다."
+    elif msg == "오늘의 경기":
         url = "https://sports.news.naver.com/kbaseball/schedule/index.nhn"
         req = requests.get(url).text
         doc = BeautifulSoup(req, "html.parser")
@@ -200,28 +237,32 @@ def message():
         home = doc.select("#todaySchedule > li > div.vs_rgt > p > strong")
         away_score = doc.select("#todaySchedule > li > div.vs_lft > strong")
         home_score = doc.select("#todaySchedule > li > div.vs_rgt > strong")
-        test = "★ 오늘의 경기결과\n\n"
+        cancel = doc.select("#todaySchedule > li")  # 각 블럭 별로 취소인지 확인할 것임
+        test = "★ 오늘의 경기\n\n"
         stadium = {"넥센":"고척", "SK":"문학", "두산":"잠실", "LG":"잠실", "KIA":"광주",
                     "롯데":"사직", "삼성":"대구", "NC":"마산", "한화":"대전", "KT":"수원"}
-        game_status = doc.select("em.state")
-        for i in range(5):
-            if game_status[i].text.strip() == "종료":
-                if home[i].text in stadium:
-                    test = test + stadium[home[i].text] + "\n" + away[i].text + "\t" + away_score[i].text + "\t:\t" + home_score[i].text + "\t" + home[i].text + "\n\n"
-                    return_msg = test[:-2]
-            else:
-                return_msg = "아직 종료된 경기가 없습니다."
+        if len(away) != 0:  # 진행되는 경기가 있으면
+            game_status = doc.select("em.state")
+            for i in range(len(away)): # away 즉, 오늘의 경기 개수
+                if len(cancel[i].select("div.cancel")) != 0:    # 경기 취소시
+                    test = test + stadium[home[i].text] + " (취소)\n" + away[i].text + "\tVS\t" + home[i].text + "(♠)\n\n"
+                else:
+                    if home[i].text in stadium:
+                        test = test + stadium[home[i].text] + " (" + game_status[i].text.strip() + ")\n" + away[i].text + "\t" + away_score[i].text + "\t:\t" + home_score[i].text + "\t" + home[i].text + "(♠)\n\n"
+            return_msg = test[:-2]
+        else:
+            return_msg = "오늘 진행되는 경기가 없습니다." 
     elif msg == "팀순위":
         url = "http://www.statiz.co.kr/league.php?opt=2018"
         req = requests.get(url).text
         doc = BeautifulSoup(req, "html.parser")
-        test = "★ 2018 KBO리그 팀순위\n\n순위\t|\t팀\t|\t경기\t|\t승\t|\t패\t|\t무\t|\t게임차\t|\t승률\n"
+        test = "★ 2018 KBO리그 팀순위\n\n순위 │ 팀 │ 경기 │ 승 │ 패 │ 무 │ 게임차 │ 승률\n\n"
         for team in range(1,11,1):
             x = doc.select("table.table.table-striped > tr")[team]
             for i in range(0,8,1):
-                test = test + x.select("td")[i].text + "\t|\t"
+                test = test + x.select("td")[i].text + " │ "
                 if i == 7:
-                    test = test[:-3] + "\n"
+                    test = test[:-3] + "\n\n"
         return_msg = test
         
     else:
@@ -258,11 +299,11 @@ def message():
     elif jeju_bool == True:
         json_return = {
             "message" : {
-                "text" : "무슨 요일 메뉴가 궁금하세요?"
+                "text" : "언제 메뉴가 궁금하세요?"
             },
             "keyboard" : {
                 "type" : "buttons",
-                "buttons" : ["오늘의 메뉴", "일", "월", "화", "수", "목", "금", "토"]
+                "buttons" : ["오늘의 메뉴", "내일의 메뉴", "일", "월", "화", "수", "목", "금", "토"]
             }
         }
     elif kbo_bool == True:
@@ -272,7 +313,7 @@ def message():
             },
             "keyboard" : {
                 "type" : "buttons",
-                "buttons" : ["오늘의 매치업", "오늘의 경기 결과", "팀순위"]
+                "buttons" : ["오늘의 매치업", "오늘의 경기", "팀순위"]
             }
         }        
     else:
@@ -289,4 +330,4 @@ def message():
     # json.dumps()와 같은 기능 - json으로 바꿔주는 기능
     return jsonify(json_return)
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
--- test2.py
+++ test2.py
@@ -7,63 +7,16 @@ req = requests.get(url).text
 doc = BeautifulSoup(req, "html.parser")
 
 #print(doc)
-info = [[],[],[],[],[],[],[],[]]
-test = "★ 2018 KBO리그 팀순위\n\n순위\t|\t팀\t|\t경기\t|\t승\t|\t패\t|\t무\t|\t게임차\t|\t승률\n"
+# print(len(doc.select("#todaySchedule > li")[0]))
+
+test = "★ 2018 KBO리그 팀순위\n\n순위 │ 팀 │ 경기 │ 승 │ 패 │ 무 │ 게임차 │ 승률\n"
 for team in range(1,11,1):
     x = doc.select("table.table.table-striped > tr")[team]
     for i in range(0,8,1):
-        info[i].append(x.select("td")[i].text)
+        test = test + x.select("td")[i].text + " │ "
         if i == 7:
             test = test[:-3] + "\n"
-
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import six
-
-df = pd.DataFrame()
-df['순위'] = info[0]
-df['팀'] = info[1]
-df['경기'] = info[2]
-df['승'] = info[3]
-df['패'] = info[4]
-df['무'] = info[5]
-df['게임차'] = info[6]
-df['승률'] = info[7]
-print(df)
-# print(doc.select("#_tab_box_kbo > div > ul > li > div.vs_list.vs_list2 > div > div")[0].text.strip())
-
-
-# away = doc.select("#todaySchedule > li > div.vs_lft > p > strong")
-# home = doc.select("#todaySchedule > li > div.vs_rgt > p > strong")
-# away_pit = doc.select("#todaySchedule > li > div.vs_lft > p > span > a")
-# home_pit = doc.select("#todaySchedule > li > div.vs_rgt > p > span > a")
-
-# test = ""
-# for i in range(5):
-#     test = test + away[i].text + "\t\tVS\t\t" + home[i].text + "\n" + away_pit[i].text + "\t\tVS\t\t" + home_pit[i].text + "\n\n"
-    
-# print(test)
-
-
-
-#print(doc.select("#mainContents > div.module-wrapper > table.table.table-list.table-bordered.table-week > tbody > tr > td > p.menu"))
-#print(doc.find("span"))
-# x = doc.select("div.badge")[0].text
-
-
-# test = "순위\t팀\tG\t승\t패\t무\t승차\t승률\n"
-# for team in range(1,11,1):
-#     x = doc.select("table.table.table-striped > tr")[team]
-#     # x = x.select("td")[1].text
-#     for i in range(0,8,1):
-#         test = test + x.select("td")[i].text + "\t"
-#         if i == 7:
-#             test = test + "\n"
-# print(test)
-# print(len(x))
-
+print(test)
 
 # title_tag = doc.select("dt.tit > a")
 # star_tag = doc.select("div.star_t1 > a > span.num")
